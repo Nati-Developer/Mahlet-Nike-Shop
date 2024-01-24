142ff545f7d3443c6f367b2b809916a549dcee72
Natnael Wosen <123094252+Nati-Developer@users.noreply.github.com>
2024-01-24 23:37:52 +0500

    Add files via upload

---
+++ src/Section/Services.js
@@ -0,0 +1,16 @@
+import ServiceCard from "../Components/ServiceCard"
+import { services } from "../constant"
+
+const Services = () => {
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {
+        services.map((service) => (
+          <ServiceCard key={service.label} {...service} />
+        ))
+      }
+    </section>
+  )
+}
+
+export default Services
